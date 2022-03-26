828538bd45938f2c36a90cab15d83980e2bfcf45
NeverMind109 <vladgladkih123@gmail.com>
2022-03-26 22:30:47 +0300

    Created: Added Header Section

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,19 @@
 $(function () {
-    
-})
+  $(function () {
+    $(".menu-mobile").on("click", function () {
+      $(".menu").toggleClass("menu--active");
+      $("body").toggleClass("overflow-hidden");
+      $(".menu-mobile").toggleClass("menu-mobile--active");
+      let link = $(".menu-list__link");
+      let logo = $(".logo");
+      function click(element) {
+        element.on("click", function () {
+          $(".menu").removeClass("menu--active");
+          $("body").removeClass("overflow-hidden");
+        });
+      }
+      click(link);
+      click(logo);
+    });
+  });
+});
