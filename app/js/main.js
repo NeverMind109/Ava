4f5b629649c1330b879b3561254e309e9c006f3f
NeverMind109 <vladgladkih123@gmail.com>
2022-03-29 19:10:05 +0300

    Created: Added Reviews Section

--- app/js/main.js
+++ app/js/main.js
@@ -34,4 +34,39 @@ $(function () {
       );
     });
   })();
+
+  $(".reviews__slider").slick({
+    dots: true,
+    infinite: true,
+    autoplay: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    prevArrow: false,
+    nextArrow: '<button class="reviews__slider-next" type="button"></button>',
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 769,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 446,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: false,
+        },
+      },
+    ],
+  });
 });
